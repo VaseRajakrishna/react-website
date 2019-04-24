23432c1d3383ce1efa4474263b958afe285acbf2
Godwin VC <godwin.k@apalya.com>
2019-04-24 18:09:32 +0530

    added homepage in package.json

---
+++ build/precache-manifest.70cd9d872d5f3aa34aea1f6cb3ad0de1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc2b33576302d210c132c0e559d5e325",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d69d225ce344154049da",
+    "url": "./static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "e3675e6698c619326ff3",
+    "url": "./static/css/main.ff00079d.chunk.css"
+  },
+  {
+    "revision": "d69d225ce344154049da",
+    "url": "./static/js/2.7015c0ee.chunk.js"
+  },
+  {
+    "revision": "e3675e6698c619326ff3",
+    "url": "./static/js/main.7e0295a0.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "dea3064787aaa67dda618e71006a0c70",
+    "url": "./static/media/contact-bg.dea30647.png"
+  },
+  {
+    "revision": "7e4520c22f2b31c378bed1d19369b13b",
+    "url": "./static/media/header-bg.7e4520c2.png"
+  },
+  {
+    "revision": "ce906c28dc62cb4eb67409c3c7d73bb4",
+    "url": "./static/media/icon3.ce906c28.png"
+  },
+  {
+    "revision": "e9b2ec4e83cb65db01389abeaa444d51",
+    "url": "./static/media/map.e9b2ec4e.png"
+  }
+]);
