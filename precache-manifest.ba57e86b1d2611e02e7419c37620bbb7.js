79ac1c0e765a52ce1c1cbcf724632ea2ce00219b
VaseRajakrishna <vaseraja888@gmail.com>
2019-04-24 15:34:13 +0530

    build folder added

---
+++ precache-manifest.ba57e86b1d2611e02e7419c37620bbb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67dbd5f4b382e664afdcf7d78a230300",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e5bc90a3adacafb0b78",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "cd20e2759bf7a9632888",
+    "url": "/static/css/main.a1ec60ed.chunk.css"
+  },
+  {
+    "revision": "5e5bc90a3adacafb0b78",
+    "url": "/static/js/2.e994cd53.chunk.js"
+  },
+  {
+    "revision": "cd20e2759bf7a9632888",
+    "url": "/static/js/main.b1ea1b96.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
